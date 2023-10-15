cbe4642c16e09da0a59ecd6e4c3fbed2c8409921
abdanzakialifian <abdan.alifian@mncbank.co.id>
2023-10-15 16:58:45 +0700

    Add vertical scroll to column

--- app/src/main/java/com/compose/codelabjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/compose/codelabjetpackcompose/MainActivity.kt
@@ -14,7 +14,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -70,7 +72,9 @@ fun TipTimeApp() {
     val tip = calculateTip(amount, tipPercent, isRoundUp)
 
     Column(
-        modifier = Modifier.padding(40.dp),
+        modifier = Modifier
+            .padding(40.dp)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
